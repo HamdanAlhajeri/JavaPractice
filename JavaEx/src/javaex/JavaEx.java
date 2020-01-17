41aee227ccdb2d881be768de45cc6555fa8d6239
662551322 <662551322@192.168.1.109>
2020-01-17 19:57:29 +0400

    Averages

--- JavaEx/src/javaex/JavaEx.java
+++ JavaEx/src/javaex/JavaEx.java
@@ -3,19 +3,33 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package javaex;
 
-/**
- *
- * @author 662551322
- */
-public class JavaEx {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class JavaEx {
     
+    
+    public static void main(String[] args) {
+        average();
+    }
+    public static void average(){
+        int sum = 0;
+        int num = 0;
+        int count = 0;
+        Scanner in = new Scanner(System.in);
+        num = in.nextInt();
+        while(num != 0){
+            num = in.nextInt();
+            sum += num;
+            count += 1;
+        } 
+        if (count == 0){
+            System.out.println("The average is undefinded");
+        }else{
+            System.out.println("The average is: " + (float)sum/count);
+        }
+
+    }
 }
