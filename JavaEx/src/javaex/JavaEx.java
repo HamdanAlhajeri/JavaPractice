f47846fe4213539cc58846db8bb41456fa8ae701
662551322 <662551322@192.168.1.109>
2020-01-17 21:14:37 +0400

    Pattern

--- JavaEx/src/javaex/JavaEx.java
+++ JavaEx/src/javaex/JavaEx.java
@@ -6,13 +6,20 @@
 
 package javaex;
 
+/**	
+ *	
+ * @author Hamdan	
+ */
+
 import java.util.Scanner;
 
 public class JavaEx {
     
     
     public static void main(String[] args) {
-        average();
+        //average();
+        //checkPrime();
+        printPattern();
     }
     public static void average(){
         int sum = 0;
@@ -30,6 +37,42 @@ public class JavaEx {
         }else{
             System.out.println("The average is: " + (float)sum/count);
         }
-
+    }
+    public static void checkPrime(){
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+                 
+        while(num != 0){
+            int i = 2;
+            while(i < num){
+                if(num % i == 0){
+                    break;
+                }
+                i++;
+            }
+            if (num == i){
+                System.out.println("This number is prime");
+            }else {
+                System.out.println("This number is not prime");
+            }
+            num = in.nextInt();
+            
+        }
+    }
+    public static void printPattern(){
+        int ilimit = 0;
+        Scanner in = new Scanner(System.in);
+        int num;
+        num = in.nextInt();
+        while(ilimit < num){
+            int i = 0;
+            while(i < ilimit){
+            System.out.print("* ");
+            i++;
+            }
+            System.out.println(" ");
+            ilimit++;
+        }
+        
     }
 }
